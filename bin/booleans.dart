55dd3fcc01ce7b022c33a7e878f8fb0c7716beb9
avrians <avriansyahb10@gmail.com>
2023-10-07 20:13:20 +0700

    [Av][Boolean&Operator]

---
+++ bin/booleans.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  bool alwaysTrue = true;
+  var alwaysFalse = false;
+  var notTrue = !true;
+  bool notFalse = !false;
+
+  if(true){
+    print('Benar');
+  } else {
+    print('Salah');
+  }
+}
