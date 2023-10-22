a2de446852b8b7bde25bceccb7e762ef7ed6df97
avrians <avriansyahb10@gmail.com>
2023-10-22 22:49:13 +0700

    [Av][Update set]

--- bin/control_flow/set.dart
+++ bin/control_flow/set.dart
@@ -1,4 +1,25 @@
 void main() {
+  // Set Default
+  Set<int> anotherSet = new Set.from([1, 4, 3, 4, 1]);
+
+  print("anotherSet: $anotherSet");
+
+  // Menambah set
+  var numberSet = {1,8,2};
+     // menambahkan data ke dalam set
+  numberSet.add(5);
+  numberSet.addAll({6,7});
+  print(numberSet);
+
+  // menghapus data dari set
+  var numberSet2 = {1,4,6, 2, 3};
+  // menghapus nilai 3 dari set
+  numberSet2.remove(3);
+  print(numberSet2);
+
+  // menampilkan data pada set index
+  print(numberSet2.elementAt(2));
+
   var setA = {1, 2, 4, 5};
   var setB = {1, 5, 7};
 
