325455fa846ef707d9d0004e11892da0abd24dd6
avrians <avriansyahb10@gmail.com>
2023-11-06 14:12:40 +0700

    [Av][Properties & Methods] Update

--- bin/animal.dart
+++ bin/animal.dart
@@ -1,21 +1,31 @@
 class Animal {
-  String name = '';
-  int age = 0;
-  double weight = 0;
+  String _name = '';
+  int _age = 0;
+  double _weight = 0;
 
-  Animal(this.name, this.age, this.weight);
+  Animal(this._name, this._age, this._weight);
 
+  // Setter
+  set name(String value) {
+    _name = value;
+  }
+
+  // Getter
+  double get weight => _weight;
+  int get age => _age;
+
+  // Methods
   void eat() {
-    print('$name is eating.');
-    weight = weight + 0.2;
+    print('$_name is eating.');
+    _weight = _weight + 0.2;
   }
 
   void sleep() {
-    print('$name is sleeping.');
+    print('$_name is sleeping.');
   }
 
   void poop() {
-    print('$name is pooping.');
-    weight = weight - 0.1;
+    print('$_name is pooping.');
+    _weight = _weight - 0.1;
   }
 }
