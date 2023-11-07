a360d95fe88eb86707d8bdb654cdf5d54d142984
avrians <avriansyahb10@gmail.com>
2023-11-07 10:46:57 +0700

    [Av][Update Constructor]

--- bin/oop/constructor.dart
+++ bin/oop/constructor.dart
@@ -1,9 +1,12 @@
 import 'hewan.dart';
 
 void main() {
-  var dicodingCat = Hewan();
+  var dicodingCat = Hewan('Gray', 2, 4.2);
 
   dicodingCat.name = 'Gray';
   dicodingCat.age = 2;
   dicodingCat.weight = 4.2;
+
+  var dicodingKucing = Hewan.cat('Gray', 4.2);
+  print(dicodingKucing.age);
 }
--- bin/oop/hewan.dart
+++ bin/oop/hewan.dart
@@ -3,4 +3,25 @@ class Hewan {
   String name = '';
   int age = 0;
   double weight = 0;
+
+  // Mendeklarasikan sebuah constructor
+  Hewan(String name, int age, double weight) {
+    this.name = name;
+    this.age = age;
+    this.weight = weight;
+  }
+
+  // Opsi lain
+  // Hewan(this.name, this.age, this.weight);
+
+  // Berikut beberapa contoh untuk mendeklarasikan
+  // Named Constructor.
+  Hewan.name(this.name);
+  Hewan.age(this.age);
+  Hewan.weight(this.weight);
+
+  // Initializer list
+  Hewan.cat(this.name, this.weight) : age = 2 {
+    print("This cat named $name is $age years old");
+  }
 }
