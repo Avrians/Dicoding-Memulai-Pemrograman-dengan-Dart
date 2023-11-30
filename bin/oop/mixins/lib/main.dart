63f4feb2a59cd21268e1066b3af8af0e963ebe40
avrians <avriansyahb10@gmail.com>
2023-11-30 20:43:07 +0700

    [av][Mixins]

--- bin/oop/mixins/lib/main.dart
+++ bin/oop/mixins/lib/main.dart
@@ -1,3 +1,44 @@
-void main(List<String> args) {
-  print("Hello");
+mixin Flyable {
+  void fly() {
+    print("I'm flying");
+  }
+}
+
+mixin Walkable {
+  void walk() {
+    print("I'm walking");
+  }
+}
+
+mixin Swimmable {
+  void swim() {
+    print("I'm swimming");
+  }void main() {
+  var arielNoah = Musician();
+  arielNoah.perform();
+}
+
+abstract class Performer {
+  void perform();
+}
+
+mixin Dancer implements Performer {
+  @override
+  void perform() {
+    print('Dancing');
+  }
+}
+
+mixin Singer implements Performer {
+  @override
+  void perform() {
+    print('Singing');
+  }
+}
+
+class Musician extends Performer with Dancer, Singer {
+  void showTime() {
+    perform();
+  }
+}
 }
