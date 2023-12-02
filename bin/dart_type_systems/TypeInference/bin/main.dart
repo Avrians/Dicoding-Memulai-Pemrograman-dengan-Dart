79cbd7027a4377faae60495b952481e72c53ff3a
avrians <avriansyahb10@gmail.com>
2023-12-02 12:54:19 +0700

    [Av][Type Inference]

---
+++ bin/dart_type_systems/TypeInference/bin/main.dart
@@ -0,0 +1,23 @@
+Map<String, dynamic> company = {'name': 'Dicoding', 'yearsFounded': 2015};
+
+var company2 = {'name': 'Dicoding', 'yearsFounded': 2015};
+
+void main() {
+  Fish fish = Shark();
+}
+
+class Animal {}
+
+// Bird
+class Bird implements Animal {}
+
+class Dove implements Bird {}
+
+class Duck implements Bird {}
+
+// Fish
+class Fish implements Animal {}
+
+class Shark implements Fish {}
+
+class FlyingFish implements Fish {}
